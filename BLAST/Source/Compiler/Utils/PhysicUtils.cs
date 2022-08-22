77ec96063d6b5bcaa4ea5647c24889846deb973d
nijnstein <96932314+nijnstein@users.noreply.github.com>
2022-08-23 00:21:36 +0200

    sync

---
+++ BLAST/Source/Compiler/Utils/PhysicUtils.cs
@@ -0,0 +1,38 @@
+﻿//############################################################################################################################
+//                                                                                                                           #
+//  ██████╗ ██╗      █████╗ ███████╗████████╗                           Copyright © 2022 Rob Lemmens | NijnStein Software    #
+//  ██╔══██╗██║     ██╔══██╗██╔════╝╚══██╔══╝                                    <rob.lemmens.s31 gmail com>                 #
+//  ██████╔╝██║     ███████║███████╗   ██║                                           All Rights Reserved                     #
+//  ██╔══██╗██║     ██╔══██║╚════██║   ██║                                                                                   #
+//  ██████╔╝███████╗██║  ██║███████║   ██║     V1.0.4e                                                                       #
+//  ╚═════╝ ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝                                                                                   #
+//                                                                                                                           #
+//############################################################################################################################
+//                                                                                                                     (__)  #
+//       Unauthorized copying of this file, via any medium is strictly prohibited proprietary and confidential         (oo)  #
+//                                                                                                                     (__)  #
+//############################################################################################################################
+#pragma warning disable CS1591
+#pragma warning disable CS0162
+
+
+#if STANDALONE_VSBUILD
+    using NSS.Blast.Standalone;
+    using System.Reflection;
+#else
+using UnityEngine;
+using Unity.Burst.CompilerServices;
+#endif
+
+
+using Unity.Burst;
+using Unity.Mathematics;
+
+using V1 = System.Single;
+using V2 = Unity.Mathematics.float2;
+using V3 = Unity.Mathematics.float3;
+using V4 = Unity.Mathematics.float4;
+
+namespace NSS.Blast.Interpretor
+{
+}
--- BLAST/Source/Compiler/Utils/mathex.cs
+++ BLAST/Source/Compiler/Utils/mathex.cs
@@ -45,16 +45,49 @@ namespace NSS.Blast
 
     public static class mathex
     {
-     
 
-            /// <summary>
-            /// Determines if the two float values are equal to each other within the range of the epsilon.
-            /// </summary>
-            /// <param name="a"></param>
-            /// <param name="b"></param>
-            /// <param name="epsilon"></param>
-            /// <returns></returns>
-            public static bool FloatEquals(float a, float b, float epsilon = 0.000001f)
+        public static float3 Project(this float3 vector, float3 normal)
+        {
+            normal = math.normalize(normal);
+            float dist = math.dot(vector, normal);
+            float3 projected = dist * normal;
+            return projected;
+        }
+        public static float3 ProjectOnPlane(this float3 vector, float3 normal)
+        {
+            float3 projected = vector - vector.Project(normal);
+            return projected;
+        }
+        public static float AngleFrom(this float3 vector1, float3 vector2)
+        {
+            return math.acos(math.dot(math.normalize(vector1), math.normalize(vector2)));
+        }
+        public static quaternion ProjectOnPlane(this quaternion rotation, float3 vector)
+        {
+            //I thought you might find this one interesting, the new quaternion has no way to convert itself to Euler angles so I made this function to get its rotation around an axis
+
+            float3 flatRotatedVector = math.mul(rotation, new float3(0, 0, 1)).ProjectOnPlane(vector);
+            float angle = 0;
+            if (math.dot(flatRotatedVector, new float3(1, 0, 0)) > 0)
+                angle = new float3(0, 0, 1).AngleFrom(flatRotatedVector);
+            else
+                angle = 2 * math.PI - new float3(0, 0, 1).AngleFrom(flatRotatedVector);
+            if (!float.IsNaN(angle))
+                return quaternion.AxisAngle(vector, angle);
+            else
+                return quaternion.identity;
+        }
+
+
+
+        /// <summary>
+        /// Determines if the two float values are equal to each other within the range of the epsilon.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="epsilon"></param>
+        /// <returns></returns>
+        public static bool FloatEquals(float a, float b, float epsilon = 0.000001f)
         {
             return math.abs(a - b) <= epsilon;
         }
